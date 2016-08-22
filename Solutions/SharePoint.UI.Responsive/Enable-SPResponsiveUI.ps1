2f60d783cf8a49624ea6d9fe49d771dc9b975eeb
SPDoctor <BillA@flosim.com>
2016-08-22 13:40:07 +0100

    Use Enable-SPOResponsiveUI cmdlet, simplified, removed inconsistencies.

--- Solutions/SharePoint.UI.Responsive/Enable-SPResponsiveUI.ps1
+++ Solutions/SharePoint.UI.Responsive/Enable-SPResponsiveUI.ps1
@@ -1,35 +1,25 @@
 ﻿<#
 .SYNOPSIS
-Enables the Responsive UI on a target SharePoint 2013 or SharePoint 2016 on-premises site collection.
+Enables the Responsive UI on a target SharePoint 2013, SharePoint 2016 on-premises or SharePoint Online site collection.
 
 .EXAMPLE
-PS C:\> .\Enable-SPResponsiveUI.ps1 -TargetSiteUrl "https://intranet.mydomain.com/sites/targetSite"
+PS C:\> .\Enable-SPResponsiveUI.ps1 -Web "https://intranet.mydomain.com/sites/targetSite"
 
 .EXAMPLE
 PS C:\> $creds = Get-Credential
-PS C:\> .\Enable-SPResponsiveUI.ps1 -TargetSiteUrl "https://intranet.mydomain.com/sites/targetSite" -InfrastructureSiteUrl "https://intranet.mydomain.com/sites/infrastructureSite" -Credentials $creds
+PS C:\> .\Enable-SPResponsiveUI.ps1 -Web "https://intranet.mydomain.com/sites/targetSite" -Credentials $creds
 #>
 [CmdletBinding()]
 param
 (
     [Parameter(Mandatory = $true, HelpMessage="Enter the URL of the target site collection, e.g. 'https://intranet.mydomain.com/sites/targetSite'")]
     [String]
-    $TargetSiteUrl,
-
-    [Parameter(Mandatory = $false, HelpMessage="Enter the URL of the infrastructural site collection, if any. It is an optional parameter. Values are like: 'https://intranet.mydomain.com/sites/infrastructureSite'")]
-    [String]
-    $InfrastructureSiteUrl,
+    $Web,
 
     [Parameter(Mandatory = $false, HelpMessage="Optional administration credentials")]
     [PSCredential]
     $Credentials
 )
-
-# If the Infrastructure Site URL is not provided, we will fallback to the Target Site URL
-if ($InfrastructureSiteUrl -eq "") 
-{
-    $InfrastructureSiteUrl = $TargetSiteUrl
-}
 
 if($Credentials -eq $null)
 {
@@ -39,18 +29,16 @@ if($Credentials -eq $null)
 Write-Host -ForegroundColor White "--------------------------------------------------------"
 Write-Host -ForegroundColor White "|               Enabling Responsive UI                 |"
 Write-Host -ForegroundColor White "--------------------------------------------------------"
-
-Write-Host -ForegroundColor Yellow "Target Site URL: $targetSiteUrl"
-Write-Host -ForegroundColor Yellow "Infrastructure Site URL: $InfrastructureSiteUrl"
+Write-Host
 
 try
 {
-    Connect-SPOnline $InfrastructureSiteUrl -Credentials $Credentials
-    Apply-SPOProvisioningTemplate -Path .\01-Responsive.UI.Infrastructure.xml -Handlers Files
-
-    Connect-SPOnline $TargetSiteUrl -Credentials $Credentials
-    Apply-SPOProvisioningTemplate -Path .\02-Responsive.UI.Template.xml -Handlers CustomActions,Features -Parameters @{"InfrastructureSiteUrl"=$InfrastructureSiteUrl}
-
+    Write-Host -ForegroundColor Yellow "Connecting to target site URL: $Web"
+    Connect-SPOnline $Web -Credentials $Credentials
+    Write-Host -ForegroundColor Yellow "Enabling responsive UI on target site"
+    Enable-SPOResponsiveUI 
+    Write-Host -ForegroundColor Yellow "Uploading custom responsive UI assets to target site"
+    Apply-SPOProvisioningTemplate -Path .\Responsive.UI.Infrastructure.xml -Handlers Files
     Write-Host -ForegroundColor Green "Responsive UI application succeeded"
 }
 catch
--- Solutions/SharePoint.UI.Responsive/SP-Responsive-UI.js
+++ Solutions/SharePoint.UI.Responsive/SP-Responsive-UI.js
@@ -63,7 +63,7 @@ PnPResponsiveApp.init = function () {
                 var currentScriptBaseUrl = currentScriptUrl.substring(0, currentScriptUrl.lastIndexOf("/") + 1);
 
                 addViewport();
-                loadCSS(currentScriptBaseUrl + 'pnp_responsive_ui.css');
+                loadCSS(currentScriptBaseUrl + 'SP-Responsive-UI.css');
             }
 
             PnPResponsiveApp.setUpToggling();
