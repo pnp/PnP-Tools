1ee9160ae3e811899fae7f3f91852cb87391827b
Bert Jansen <bjansen@microsoft.com>
2018-03-29 12:27:13 +0200

    Addition of test project for modernization framework

---
+++ Solutions/SharePoint.Modernization/SharePoint.Modernization.Framework.Tests/AdHocTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.SharePoint.Client;
+
+namespace SharePoint.Modernization.Framework.Tests
+{
+    [TestClass]
+    public class AdHocTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var cc = TestCommon.CreateClientContext("https://bertonline.sharepoint.com/sites/bert1"))
+            {
+                cc.Web.EnsureProperty(p => p.Description);
+                Assert.IsNotNull(cc.Web.Description);
+            }
+
+        }
+    }
+}
---
+++ Solutions/SharePoint.Modernization/SharePoint.Modernization.Framework.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SharePoint.Modernization.Framework.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SharePoint.Modernization.Framework.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("07492d38-21de-4d09-a07d-aa1369819464")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
