6631e65f0ebe1ac6096bce4f6c0c412d1a8decc9
Bert Jansen <bjansen@microsoft.com>
2018-04-27 16:19:56 +0200

    Reorder webparts in model

--- Solutions/SharePoint.Modernization/SharePoint.Modernization.Framework/Transform/PageTransformator.cs
+++ Solutions/SharePoint.Modernization/SharePoint.Modernization.Framework/Transform/PageTransformator.cs
@@ -137,7 +137,7 @@ namespace SharePoint.Modernization.Framework.Transform
             {
                 pageData = new WikiPage(pageTransformationInformation.SourcePage, pageTransformation).Analyze();
 
-                // Wiki pages can contain embedded images and videos, which is not supported the target RTE...split wiki text blocks so the transformator can handle the images and videos as separate web parts
+                // Wiki pages can contain embedded images and videos, which is not supported by the target RTE...split wiki text blocks so the transformator can handle the images and videos as separate web parts
                 if (pageTransformationInformation.HandleWikiImagesAndVideos)
                 {
                     pageData = new Tuple<PageLayout, List<WebPartEntity>>(pageData.Item1, new WikiTransformator().Transform(pageData.Item2));
