0f71610c4d0f1271cd32ab47c23938930aed728a
Bert Jansen <bjansen@microsoft.com>
2018-05-04 18:18:49 +0200

    Upgrade to May 2018 pnp nuget package

--- Solutions/SharePoint.Modernization/SharePoint.Modernization.Framework.Tests/Transform/Tests.cs
+++ Solutions/SharePoint.Modernization/SharePoint.Modernization.Framework.Tests/Transform/Tests.cs
@@ -70,7 +70,7 @@ namespace SharePoint.Modernization.Framework.Tests.Transform
                 //demo1
                 //wikitext
                 //wiki_li
-                var pages = cc.Web.GetPages("demo17.aspx");
+                var pages = cc.Web.GetPages("webparts.aspx");
 
                 foreach (var page in pages)
                 {
