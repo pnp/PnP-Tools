20ef615f82e309478ae0dfabf97040fb350ac688
Bert Jansen <bjansen@microsoft.com>
2018-05-16 16:54:07 +0200

    namespace alignement + updates

--- Solutions/SharePoint.Modernization/Scripts/PageTransformation/TransformPageSample.ps1
+++ Solutions/SharePoint.Modernization/Scripts/PageTransformation/TransformPageSample.ps1
@@ -1,5 +1,5 @@
 
-function Load-PnPModernizationFramework
+function Use-PnPModernizationFramework
 {
     param(
         [string] $PathToModernizationBinaries
@@ -11,7 +11,7 @@ function Load-PnPModernizationFramework
 
     process
     {
-        Add-Type -Path "$PathToModernizationBinaries\SharePoint.Modernization.Framework.dll"
+        Add-Type -Path "$PathToModernizationBinaries\SharePointPnP.Modernization.Framework.dll"
     }
 
     end
@@ -20,14 +20,16 @@ function Load-PnPModernizationFramework
     }
 }
 
-function Execute-PnPModernizationPageTransformation
+function Invoke-PnPModernizationPageTransformation
 {
     param(
         [string] $PathToModernizationBinaries,
+        [string] $WebPartMappingFile = $null,
         $Page,
         [bool] $Overwrite = $false,
         [bool] $HandleWikiImagesAndVideos = $true,
-        [bool] $ReplaceHomePageWithDefaultHomePage = $false
+        [bool] $ReplaceHomePageWithDefaultHomePage = $false,
+        [bool] $TargetPageTakesSourcePageName = $false
     )
 
     begin
@@ -41,13 +43,23 @@ function Execute-PnPModernizationPageTransformation
         try 
         {            
             # Create the PageTransformationInformation object and populate it
-            $pageTransformationInformation = New-Object -TypeName SharePoint.Modernization.Framework.Transform.PageTransformationInformation -ArgumentList $Page
+            $pageTransformationInformation = New-Object -TypeName SharePointPnP.Modernization.Framework.Transform.PageTransformationInformation -ArgumentList $Page
             $pageTransformationInformation.Overwrite = $Overwrite
             $pageTransformationInformation.HandleWikiImagesAndVideos = $HandleWikiImagesAndVideos
             $pageTransformationInformation.ReplaceHomePageWithDefaultHomePage = $ReplaceHomePageWithDefaultHomePage
+            $pageTransformationInformation.TargetPageTakesSourcePageName = $TargetPageTakesSourcePageName
 
             # Instantiate the page transformator
-            $pageTransformator = New-Object -TypeName SharePoint.Modernization.Framework.Transform.PageTransformator -ArgumentList $Page.Context, "$PathToModernizationBinaries\webpartmapping.xml"
+            $pageTransformator = $null
+            if ($WebPartMappingFile -ne "")
+            {
+                $pageTransformator = New-Object -TypeName SharePointPnP.Modernization.Framework.Transform.PageTransformator -ArgumentList $Page.Context, "$WebPartMappingFile"
+            }
+            else 
+            {
+                $pageTransformator = New-Object -TypeName SharePointPnP.Modernization.Framework.Transform.PageTransformator -ArgumentList $Page.Context, "$PathToModernizationBinaries\webpartmapping.xml"
+            }
+
             # Transform
             $pageTransformator.Transform($pageTransformationInformation)  
         }
@@ -72,23 +84,28 @@ function Execute-PnPModernizationPageTransformation
 # variables
 $CAMLQueryByExtension = "<View Scope='Recursive'><Query><Where><Contains><FieldRef Name='File_x0020_Type'/><Value Type='text'>aspx</Value></Contains></Where></Query></View>"
 $CAMLQueryByExtensionAndName = "<View Scope='Recursive'><Query><Where><And><Contains><FieldRef Name='File_x0020_Type'/><Value Type='text'>aspx</Value></Contains><BeginsWith><FieldRef Name='FileLeafRef'/><Value Type='text'>{0}</Value></BeginsWith></And></Where></Query></View>"
-$binaryFolder = "C:\github\BertPnPTools\Solutions\SharePoint.Modernization\SharePoint.Modernization.Framework\bin\Debug"
+$binaryFolder = "C:\github\BertPnPTools\Solutions\SharePoint.Modernization\SharePointPnP.Modernization.Framework\bin\Debug"
 
 # Load the SharePoint Modernization framework
-Load-PnPModernizationFramework $binaryFolder
+Use-PnPModernizationFramework -PathToModernizationBinaries $binaryFolder
 
 # Connect to site
 Connect-PnPOnline -Url https://bertonline.sharepoint.com/sites/espctest2 -Verbose
 
-# Get aspx page(s)
+# Get all pages
+# [string] $query = $CAMLQueryByExtension
+
+# Get specific aspx page(s)
 [string] $query = [string]::Format($CAMLQueryByExtensionAndName, "contentbyquery.aspx")
+
+# Load the pages
 $pages = Get-PnPListItem -List SitePages -Query $query 
 
 # Modernize the found pages
 foreach($page in $pages)
 {
     Write-Host "Modernizing " $page.FieldValues["FileLeafRef"] "..."    
-    if (Execute-PnPModernizationPageTransformation $binaryFolder $page $true)
+    if (Invoke-PnPModernizationPageTransformation -Page $page -WebPartMappingFile "$binaryFolder\webpartmapping.xml" -Overwrite $true)
     {
         Write-Host "Done!" -ForegroundColor Green
     }
