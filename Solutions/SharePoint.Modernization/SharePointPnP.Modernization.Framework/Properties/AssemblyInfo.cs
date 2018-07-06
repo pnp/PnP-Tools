c303b279b00b603ca3637131023bcf3ac12409ac
Bert Jansen <bjansen@microsoft.com>
2018-07-06 15:16:32 +0200

    Prep for new preview release

--- Solutions/SharePoint.Modernization/SharePointPnP.Modernization.Framework/Properties/AssemblyInfo.cs
+++ Solutions/SharePoint.Modernization/SharePointPnP.Modernization.Framework/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1805.3")]
-[assembly: AssemblyFileVersion("0.1.1805.3")]
+[assembly: AssemblyVersion("0.1.1807.0")]
+[assembly: AssemblyFileVersion("0.1.1807.0")]
 
 [assembly: InternalsVisibleTo("SharePointPnP.Modernization.Framework.Tests")]
