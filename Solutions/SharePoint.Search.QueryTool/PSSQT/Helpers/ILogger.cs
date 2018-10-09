0e9734deb4a97e33bb239651ebd87acc090e865d
Frode Sivertsen <frode.sivertsen@outlook.com>
2018-10-09 09:50:26 -0700

    checkpoint

---
+++ Solutions/SharePoint.Search.QueryTool/PSSQT/Helpers/ILogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Just create a very specific minimalistic logger interface so that I can log by accessing a static logger on the Cmdlet
+
+namespace PSSQT.Helpers
+{
+    public enum LogLevel
+    {
+        Debug,
+        Verbose,
+        Warning,
+        Error,
+        Critical,
+        None
+    }
+
+    interface ILogger
+    {
+
+        void Log(LogLevel level, string message);
+    }
+}
